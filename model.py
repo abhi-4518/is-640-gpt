0017389aa6b09baec1caec550667996f140be499
*abhi-4518* <*nirmalabhishek505@gmail.com*>
2024-12-08 01:14:07 -0800

    Removed LayerNorm 4* input

--- model.py
+++ model.py
@@ -44,9 +44,9 @@ class FeedForward(nn.Module):
     def __init__(self, n_embd, dropout=0.2):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(n_embd, 4 * n_embd),
+            nn.Linear(n_embd,n_embd),
             nn.ReLU(),
-            nn.Linear(4 * n_embd, n_embd),
+            nn.Linear(n_embd, n_embd),
             nn.Dropout(dropout),
         )
 

8b2e888a08ebf38982e275327cb6df654a5150ab
*abhi-4518* <*nirmalabhishek505@gmail.com*>
2024-12-09 00:36:13 -0800

    Changed from character level processing to word level proicessing

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-import torch
+"""import torch
 
 class Data:
     def __init__(self, file_path, device='cpu', block_size=256):
@@ -13,6 +13,41 @@ class Data:
         
         self.encode = lambda s: [self.stoi[c] for c in s]
         self.decode = lambda l: ''.join([self.itos[i] for i in l])
+
+        self.data = torch.tensor(self.encode(self.text), dtype=torch.long)
+        self.block_size = block_size
+        self.device = device
+
+    def split_data(self, split_ratio=0.9):
+        n = int(split_ratio * len(self.data))
+        train_data = self.data[:n]
+        val_data = self.data[n:]
+        return train_data, val_data
+
+    def get_batch(self, split, batch_size, train_data, val_data):
+        data = train_data if split == 'train' else val_data
+        ix = torch.randint(len(data) - self.block_size, (batch_size,))
+        x = torch.stack([data[i:i+self.block_size] for i in ix])
+        y = torch.stack([data[i+1:i+self.block_size+1] for i in ix])
+        x, y = x.to(self.device), y.to(self.device)
+        return x, y
+"""
+import torch
+
+class Data:
+    def __init__(self, file_path, device='cpu', block_size=256):
+        with open(file_path, 'r', encoding='utf-8') as f:
+            self.text = f.read()
+
+        # Word-level tokenization
+        self.words = self.text.split()
+        self.vocab = sorted(set(self.words))
+        self.vocab_size = len(self.vocab)
+        self.stoi = {word: i for i, word in enumerate(self.vocab)}
+        self.itos = {i: word for i, word in enumerate(self.vocab)}
+        
+        self.encode = lambda s: [self.stoi[word] for word in s.split()]
+        self.decode = lambda l: ' '.join([self.itos[i] for i in l])
 
         self.data = torch.tensor(self.encode(self.text), dtype=torch.long)
         self.block_size = block_size
